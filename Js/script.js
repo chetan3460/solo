5dbff106b789df557554cc46f59ef195aad3044c
code <chetan.dmc@gmail.com>
2019-04-30 11:52:19 +0530

    Building preloader for website

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+/*================================
+        Preloader JS
+=================================*/
+$(window).on('load',function(){ 
+    $('#status').fadeOut();
+    $('#preloder').delay(350).fadeOut();
+});
