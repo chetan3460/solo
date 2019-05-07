60b37ce606e50409ac1ed05b50d0d71068c602ba
code <chetan.dmc@gmail.com>
2019-05-07 12:26:07 +0530

    Building Portfolio Section

--- Js/script.js
+++ Js/script.js
@@ -48,3 +48,40 @@ $(function() {
     });
 
 });
+
+// Portfolio
+$(window).on('load',function(){
+    // Intalize Isotope
+    $("#isotope-container").isotope({
+
+    });
+    //filter items on button click
+    $("#isotope-filters").on('click','button',function(){
+        
+        //get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter:filterValue
+        });
+
+        // active button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+
+    });
+});
+
+// Magnifier
+$(function() {
+
+    $("#portfolio-wrapper").magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
